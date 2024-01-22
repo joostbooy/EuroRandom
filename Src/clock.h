6091b8c89d8c6e5ceaa558114c0800cedc9e9c19
joostbooy <joostbooy@gmail.com>
2024-01-22 07:44:20 +0100

    more work on clock

--- Src/clock.h
+++ Src/clock.h
@@ -1,40 +1,78 @@
 #ifndef Clock_h
 #define Clock_h
 
+#include "micros.h"
+
 class Clock {
 
 public:
 
-	void update() {
-		for (int i = 0; i < kNumClocks; ++i) {
-			bool state = read(i);
+	void init() {
+		curr_tick = 0;
 
-			if (state == 1 && last_state_[i] == 0) {
-				last_state_[i] = state;
-				interval_[i] = curr_tick - last_update_[i];
-				last_update_[i] = mircos.read();
+		for (size_t i = 0; i < kNumClocks; ++i) {
+			curr_state_[i] = 0;
+			last_state_[i] = 0;
+			interval_[i] = 0;
+			last_tick_[i] = 0;
+		}
+	}
+
+	void poll() {
+		for (size_t i = 0; i < kNumClocks; ++i) {
+			last_state_[i] = curr_state_[i];
+			curr_state_[i] = read(i);
+
+			if (curr_state_[i] == 1 && last_state_[i] == 0) {
+				interval_[i] = curr_tick - last_tick_[i];
+				last_tick_[i] = curr_tick;
 			}
 		}
+
+		++curr_tick;
 	}
 
 	uint32_t interval(int port) {
 		return interval_[port];
 	}
 
+	bool current_state(int port) {
+		return curr_state_[port];
+	}
+
 private:
 
 	static const size_t kNumClocks = 2;
+
 	bool last_state_[kNumClocks];
+	bool curr_state_[kNumClocks];
+
 	uint32_t interval_[kNumClocks];
-	uint32_t last_update_[kNumClocks];
+	uint32_t last_tick_[kNumClocks];
+	uint32_t curr_tick = 0;
 
 	inline bool read(int port) {
-		if (port == 0) {
-			return !(GPIOA->BSRR & GPIO_PIN_7);
-		} else {
-			return !(GPIOA->BSRR & GPIO_PIN_4);
+		switch (port)
+		{
+		case 0:	return !(GPIOA->BSRR & GPIO_PIN_7);
+		case 1:	return !(GPIOA->BSRR & GPIO_PIN_4);
+		default:
+			break;
 		}
+		return 0;
 	}
+
 };
+inline float curve(float shape) {
+	uint16_t intergral = static_cast<uint16_t>(phase_);
+	float fractional = phase_ - intergral;
+
+	const float *lut_a = &lut_exp[intergral];
+	const float *lut_b = &lut_inv_exp[intergral];
+	float a = Dsp::cross_fade(*lut_a, *(lut_a + 1), fractional);
+	float b = Dsp::cross_fade(*lut_b, *(lut_b + 1), fractional);
+
+	return Dsp::cross_fade(a, b, shape);
+}
 
 #endif
