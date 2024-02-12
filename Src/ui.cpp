b4b3bfb012805cd877f3fb51c4f27e9691e0232a
joostbooy <joostbooy@gmail.com>
2024-02-12 09:35:06 +0100

    added pot & switch filtering in ui class

--- Src/ui.cpp
+++ Src/ui.cpp
@@ -1,15 +1,45 @@
 #include "ui.h"
 
 void Ui::init() {
+	for (int i = 0; i < NUM_SWITCHES; ++i) {
+		sw_state_[i] = 0;
+	}
 
+	for (int i = 0; i < NUM_POTS; ++i) {
+		pot_value_[i] = 0.f;
+	}
+}
+
+void Ui::debounce(SwitchId id, bool state) {
+	debounce_[id] <<= 1;
+	if (state) {
+		debounce_[id] |= 1;
+	}
+
+	if (debounce_[id] == 0x00 || debounce_[id] == 0xFF) {
+		sw_state_[id] = debounce_[id];
+	}
 }
 
 void Ui::poll() {
-	//event_que.write({BUTTON, 1});
+	debounce(BURST_INSERT, gateIo.read_burst_insert());
+	debounce(TRIGGER_INSERT, gateIo.read_trigger_insert());
+
+	if (adc.ready()) {
+		int channel = adc.curr_channel();
+		float in = adc.read() / 4095.f;
+		float out = pot_value_[channel];
+
+		pot_value_[channel] = Dsp::one_pole_filter(in, out, 0.05f);
+
+		adc.convert_next_channel();
+	}
 }
 
-void Ui::process() {
-	while (event_que.readable()) {
-		//Event e = event_que.read();
-	}
+float Ui::read_pot(PotId id) {
+	return pot_value_[id];
+}
+
+bool Ui::read_switch(SwitchId id) {
+	return sw_state_[id];
 }
--- Src/ui.h
+++ Src/ui.h
@@ -2,26 +2,48 @@
 #define Ui_h
 
 #include "que.h"
+#include "adc.h"
+#include "dsp.h"
+#include "gateIo.h"
 
 class Ui {
 
 public:
 
-	enum Type {
-		BUTTON,
-		GATE,
+	enum SwitchId {
+		BURST_INSERT,
+		TRIGGER_INSERT,
+
+		NUM_SWITCHES
 	};
+
+	enum PotId {
+		SKEW_AMMOUNT,
+		TRIANGLE_FILL,
+		TRIANGLE_DEPTH,
+		SKEW_FILL,
+		PULSE_FILL,
+		CURVE_SHAPE,
+		PULSE_WIDTH,
+		CURVE_FILL,
+		ACCENTS,
+		SHIFT,
+
+		NUM_POTS
+	};
+
 
 	void init();
 	void poll();
-	void process();
+	float read_pot(PotId);
+	bool read_switch(SwitchId);
 
 private:
-	struct Event {
-		Type type;
-		int id;
-	};
-	Que<Event, 16>event_que;
+	float pot_value_[NUM_POTS];
+	bool sw_state_[NUM_SWITCHES];
+	uint8_t debounce_[NUM_SWITCHES];
+
+	void debounce(SwitchId, bool);
 };
 
 #endif
