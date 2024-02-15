3385dd08a029dc6e0dffad4779104542c9d1e33c
joostbooy <joostbooy@gmail.com>
2024-02-15 15:01:32 +0100

    added hysterisis filteR

--- Src/clockReader.h
+++ Src/clockReader.h
@@ -1,8 +1,6 @@
 #ifndef ClockReader_h
 #define ClockReader_h
 
-//#include "timer.h"
-
 class ClockReader {
 
 public:
@@ -14,6 +12,9 @@ public:
 		last_state_ = 0;
 		last_tick_ = 0;
 		interval_ = 48000;
+
+		count_ = 0;
+		average_ = 0;
 	}
 
 	void tick(bool state) {
@@ -21,6 +22,8 @@ public:
 		curr_state_ = state;
 
 		if (curr_state_ == 1 && last_state_ == 0) {
+			//average(curr_tick - last_tick_);
+			//hysterisis(curr_tick - last_tick_, 48);
 			interval_ = curr_tick - last_tick_;
 			last_tick_ = curr_tick;
 		}
@@ -37,7 +40,25 @@ private:
 
 	uint32_t interval_;
 	uint32_t last_tick_;
-	uint32_t curr_tick = 0;
+	uint32_t curr_tick;
+
+	uint8_t count_;
+	uint32_t average_;
+
+	inline void average(uint32_t value) {
+		average_ += value;
+		if ((++count_ % 4) == 3) {
+			interval_ = average_ / 4;
+			average_ = 0;
+		}
+	}
+
+	inline void hysterisis(uint32_t value, const uint32_t step_size) {
+		if (stmlib::difference(value, interval_) >= step_size) {
+			interval_ = value;
+		}
+	}
+
 };
 
 #endif
