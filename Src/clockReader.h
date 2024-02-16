c829752881fa6b5a8c891facc08450b3a3199d50
joostbooy <joostbooy@gmail.com>
2024-02-16 12:12:13 +0100

    cleanup

--- Src/clockReader.h
+++ Src/clockReader.h
@@ -12,9 +12,6 @@ public:
 		last_state_ = 0;
 		last_tick_ = 0;
 		interval_ = 100;
-
-		count_ = 0;
-		average_ = 0;
 	}
 
 	void tick(bool state) {
@@ -22,7 +19,10 @@ public:
 		curr_state_ = state;
 
 		if (curr_state_ == 1 && last_state_ == 0) {
-			interval_ = stmlib::clip(8, 2000, curr_tick - last_tick_);
+			uint32_t new_interval_ = curr_tick - last_tick_;
+			if (new_interval_ >= 8 && new_interval_ <= 2000) {
+				interval_ = new_interval_;
+			}
 			last_tick_ = curr_tick;
 
 		}
@@ -40,17 +40,6 @@ private:
 	uint32_t interval_;
 	uint32_t last_tick_;
 	uint32_t curr_tick;
-
-	uint8_t count_;
-	uint32_t average_;
-
-	inline void average(uint32_t value) {
-		average_ += value;
-		if ((++count_ % 4) == 3) {
-			interval_ = average_ / 4;
-			average_ = 0;
-		}
-	}
 };
 
 #endif
