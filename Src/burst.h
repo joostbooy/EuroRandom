a194bdf4af819f41c09082f9fb0d2d04630cfbea
joostbooy <joostbooy@gmail.com>
2024-02-19 10:46:03 +0100

    implemented burst

--- Src/burst.h
+++ Src/burst.h
@@ -8,23 +8,49 @@ class Burst {
 public:
 
 	void init() {
-		duration_ = 0;
+
 	}
 
 	void reset(bool accent, uint32_t duration) {
+		phase_ = 0.f;
+		inc_ = accent ? (1.f / 64.f) : (1.f / 32.f);
 
+		duration_ = 0;
+		total_used_ = 0;
+		total_ = duration;
 	}
 
 	bool tick() {
+		while (duration_ == 0 && phase_ < 1.f) {
+			duration_ = next_duration();
+		}
+
 		if (duration_ > 0) {
-			--duration;
+			--duration_;
 			return true;
 		}
 		return false;
 	}
 
 private:
-	int duration_;
+	uint32_t total_;
+	uint32_t total_used_;
+	uint32_t duration_;
+
+	float inc_;
+	float phase_;
+
+	uint32_t next_duration() {
+		uint32_t next;
+
+		phase_ += inc_;
+		next = total_ * (phase_ * phase_) * RandomGenerator::next(1.f - inc_, 1.f, false);
+		next -= total_used_;
+		total_used_ += next;
+
+		return next;
+	}
+
 };
 
 #endif
