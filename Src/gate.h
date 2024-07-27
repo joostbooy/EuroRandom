80ec013c58aa6435b4f3b973a35018decf9cd195
joostbooy <joostbooy@gmail.com>
2024-07-27 12:35:57 +0200

    refactored gate

--- Src/gate.h
+++ Src/gate.h
@@ -12,11 +12,7 @@ public:
 	}
 
 	void set(bool accent, uint32_t duration) {
-		if (accent) {
-			duration_ = duration > 1 ? duration - 1 : 1;
-		} else {
-			duration_ = duration * RandomGenerator::next(0.0f, 0.65f);
-		}
+		duration_ = duration * RandomGenerator::next(accent);
 	}
 
 	bool tick() {
